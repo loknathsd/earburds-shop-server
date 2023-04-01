006921247c4dd88702a00e70f0da081700c42564
loknathsd <loknathsds66@gmail.com>
2023-04-01 14:13:59 +0600

    product update and delete done

--- src/Controller/Product/productController.js
+++ src/Controller/Product/productController.js
@@ -35,26 +35,28 @@ const getOne = async (req, res) => {
         throw new Error("Something went wrong")
     }
 }
-const update = async (req, res) => {
+const updateProduct = async (req,res) => {
+    const productId = req.params.id;
+    const updatedData = req.body;
     try {
-
+      const updatedProduct = await Product.findOneAndUpdate({ _id: productId }, updatedData);
+      res.status(200).json({ message: 'Product updated successfully'});
     } catch (error) {
-
+      console.error(error);
+      res.status(500).json({ message: 'Failed to update product' });
     }
-}
+  };
 
+//delete product
 const deleteProduct = async (req, res) => {
     try {
         await Product.deleteOne({ _id: req.params.id });
-        res.json(200).json({ message: "Product is deleted Successfully !!" })
-
+        res.status(200).json({ message: "Product is deleted Successfully !!" });
     } catch (e) {
         console.log(e);
         res.status(500).json({ message: "Product is not deleted . failed !" })
-
     }
-
 }
 
 
-module.exports = { addProduct, getAll, getOne, update, deleteProduct }
+module.exports = { addProduct, getAll, getOne, updateProduct, deleteProduct }
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const productRouter = express.Router();
-const { addProduct,getAll, getOne, deleteProduct } = require("../Controller/Product/productController");
+const { addProduct,getAll, getOne, deleteProduct, updateProduct } = require("../Controller/Product/productController");
 
 
 // add product router
 productRouter.post("/add",addProduct);
 productRouter.get("/",getAll);
 productRouter.get("/:id",getOne);
-productRouter.delete("/:id",deleteProduct)
-
-
+productRouter.delete("/:id",deleteProduct);
+productRouter.put("/edit/:id",updateProduct);
 
 module.exports =  productRouter ;
