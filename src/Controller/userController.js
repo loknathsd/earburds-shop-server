597b29d4af951ad1d5ae4564c9883c57d40933cc
loknathsd <loknathsds66@gmail.com>
2023-04-04 00:47:26 +0600

    some changes on user controller

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -36,7 +36,7 @@ const signin = async (req, res) => {
       if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
       const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
       if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
-      const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: 60 * 60 });
+      const token = jwt.sign({user:oldUser}, secret, { expiresIn: 60*60 });
       res.status(200).json({ result: oldUser, token });
     } catch (err) {
       res.status(500).json({ message: "Something went wrong" });
